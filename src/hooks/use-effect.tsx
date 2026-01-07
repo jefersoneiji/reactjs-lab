142550be56c7108cfc73a954e2b39a7c6ae861b0
Jeferson Eiji <jeferson.eiji@icloud.com>
2026-01-07 18:34:09 -0300

    feat(useEffect): added connecting to an external source with custom hook example

--- src/hooks/use-effect.tsx
+++ src/hooks/use-effect.tsx
@@ -5,6 +5,7 @@ export const UseEffectLab = () => {
         <>
             <h2>UseEffect Lab</h2>
             <SyncToExternalSource />
+            <SyncToExternalSourceCustomHook />
         </>
     );
 };
@@ -38,6 +39,55 @@ const ChatRoom = ({ room_id }: { room_id: string; }) => {
                 <input value={server_url} onChange={e => setServerUrl(e.target.value)} />
             </label>
             <h4>Welcome to the {room_id} room!</h4>
+        </>
+    );
+};
+
+const useChatRoom = ({ server_url, room_id }: { server_url: string, room_id: string; }) => {
+    useEffect(() => {
+        const connection = create_connection(server_url, room_id);
+        connection.connect();
+        return () => connection.disconnect();
+    }, [room_id, server_url]);
+};
+
+const ChatRoomWithCustomHook = ({ room_id }: { room_id: string; }) => {
+    const [server_url, setServerUrl] = useState('https://localhost:1234');
+
+    useChatRoom({ server_url, room_id });
+
+    return (
+        <>
+            <label>
+                Server URL: {' '}
+                <input value={server_url} onChange={e => setServerUrl(e.target.value)} />
+            </label>
+            <h4>Welcome to the {room_id} room!</h4>
+        </>
+    );
+};
+
+const SyncToExternalSourceCustomHook = () => {
+    const [room_id, setRoomId] = useState('general');
+    const [show, setShow] = useState(false);
+
+    return (
+        <>
+            <h3>Connect to an external source (with custom hook)</h3>
+            <label>
+                Choose the chat room: {" "}
+                <select value={room_id} onChange={e => setRoomId(e.target.value)}>
+                    <option value="general">general</option>
+                    <option value="travel">travel</option>
+                    <option value="music">music</option>
+                </select>
+            </label>
+            {" "}
+            <button onClick={() => setShow(!show)}>
+                {show ? 'Close chat' : 'Open chat'}
+            </button>
+            {show && <hr />}
+            {show && <ChatRoomWithCustomHook room_id={room_id} />}
         </>
     );
 };
