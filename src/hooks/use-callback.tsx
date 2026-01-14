06a1e5697774a47258bbbabdb18e781238b51963
Jeferson Eiji <jeferson.eiji@icloud.com>
2026-01-14 15:20:44 -0300

    feat(useCallback): added updating state from a memoized callback example

--- src/hooks/use-callback.tsx
+++ src/hooks/use-callback.tsx
@@ -1,12 +1,13 @@
 import './use-callback.css';
 
-import { memo, useCallback, useState, type FormEvent } from "react";
+import { memo, useCallback, useEffect, useRef, useState, type FormEvent } from "react";
 
 export const UseCallbackLab = () => {
     return (
         <>
             <h2>UseCallback Lab</h2>
             <PreventComponentReRenders />
+            <UpdatingStateFromAMemoizedCallback />
         </>
     );
 };
@@ -49,7 +50,7 @@ const ShippingForm = memo(({ onSubmit }: { onSubmit: (input: order_details) => v
 
     function handle_submit(e: FormEvent<HTMLFormElement>) {
         e.preventDefault();
-        
+
         const form_data = new FormData(e.currentTarget);
         const order_details = {
             ...Object.fromEntries(form_data),
@@ -104,3 +105,47 @@ function post(url: string, data: { referrer: string, order_details: any; }) {
     console.log('POST/ ' + url);
     console.log('data is: ', data);
 }
+
+interface Todo {
+    id: number,
+    text: string;
+}
+
+let next_id = 1;
+
+const TodoList = () => {
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    const prev_fn_ref = useRef<(text: string) => void | null>(null);
+
+    const handle_add_todo = useCallback((text: string) => {
+        const new_todo = { id: next_id++, text };
+        setTodos(todos => [...todos, new_todo]);
+    }, []);
+
+    useEffect(() => {
+        console.log(prev_fn_ref.current === handle_add_todo ? "'handle_add_todo: SAME reference" : "'handle_add_todo: NEW reference");
+        prev_fn_ref.current = handle_add_todo;
+    });
+
+    return (
+        <div>
+            <button onClick={() => handle_add_todo('new todo')}>
+                Add todo
+            </button>
+
+            <ul>
+                {todos.map(todo => (<li key={todo.id}>{todo.text}</li>))}
+            </ul>
+        </div>
+    );
+};
+
+const UpdatingStateFromAMemoizedCallback = () => {
+    return (
+        <>
+            <h3>Updating state from a memoized callback</h3>
+            <TodoList />
+        </>
+    );
+};
