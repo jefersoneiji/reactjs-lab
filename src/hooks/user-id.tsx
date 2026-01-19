b465185ce60193c8a1227fc8803d217bce3217b0
Jeferson Eiji <jeferson.eiji@icloud.com>
2026-01-19 19:35:05 -0300

    feat(useId): added unique id for related elements example

--- src/hooks/user-id.tsx
+++ src/hooks/user-id.tsx
@@ -5,6 +5,7 @@ export const UseIdLab = () => {
         <>
             <h2>UseId Lab</h2>
             <GenerateUniqueIDForAccessibility />
+            <GenerateIDForRelatedElements />
         </>
     );
 };
@@ -29,12 +30,29 @@ const GenerateUniqueIDForAccessibility = () => {
     return (
         <>
             <h3>Generate Unique ID for accessibility attributes</h3>
-            
+
             <h4>Choose password</h4>
             <PasswordField />
-            
+
             <h4>Confirm Password</h4>
             <PasswordField />
+        </>
+    );
+};
+
+const GenerateIDForRelatedElements = () => {
+    const id = useId();
+    return (
+        <>
+            <h3>Generate Unique ID for related elements</h3>
+
+            <form>
+                <label htmlFor={id + 'firstName'}>First Name:</label>
+                <input id={id + 'firstName'} type='text' />
+                <hr />
+                <label htmlFor={id + 'lastName'}>Last Name:</label>
+                <input id={id + 'lastName'} type='text' />
+            </form>
         </>
     );
 };
