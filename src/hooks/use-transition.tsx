d83a2c7e4f912a6b6a41e90be23afb18687c53d9
Jeferson Eiji <jeferson.eiji@icloud.com>
2026-04-01 15:48:16 -0300

    feat(useTransition): added exposing action prop from components  example

--- src/hooks/use-transition.tsx
+++ src/hooks/use-transition.tsx
@@ -1,10 +1,11 @@
-import { startTransition, useState, useTransition } from "react"
+import { memo, startTransition, useState, useTransition } from "react"
 
 export const UseTransitionLab = () => {
     return (
         <>
             <h2>Use Transition Lab</h2>
             <PerformNonBlockingUpdatesWithActions />
+            <ExposingActionPropFromComponents />
         </>
     )
 }
@@ -101,8 +102,102 @@ const PerformNonBlockingUpdatesWithActions = () => {
     return (
         <>
             <h3>Perform non-blocking updates with Actions</h3>
-
             <UpdateTotal />
+        </>
+    )
+}
+
+const ContactTab = () => {
+    return (
+        <>
+            <p>
+                You can find me online here:
+            </p>
+            <ul>
+                <li>admin@mysite.com</li>
+                <li>+1234566789</li>
+            </ul>
+        </>
+    )
+}
+
+const SlowPost = ({ index }: { index: number }) => {
+    let startTime = performance.now()
+    while (performance.now() - startTime < 1) { }
+
+    return (
+        <li style={{}}>
+            Post #{index + 1}
+        </li>
+    )
+}
+
+const PostsTab = memo(() => {
+    console.log('[ARTIFICIALLY SLOW] Rendering 500 <SlowPost/>')
+
+    let items = []
+    for (let i = 0; i < 500; i++) {
+        items.push(<SlowPost key={i} index={i} />)
+    }
+    return <ul style={{}}>{items}</ul>
+})
+
+const AboutTab = () => {
+    return <p>Welcome to my profile!</p>
+}
+
+const TabButton = ({ action, children, isActive }: { action: () => void | Promise<void>; children: React.ReactNode; isActive: boolean }) => {
+    const [isPending, startTransaction] = useTransition()
+
+    if (isActive) {
+        return <b>{children}</b>
+    }
+
+    if (isPending) {
+        return <b style={{}}>{children}</b>
+    }
+
+    return (
+        <button onClick={async () => {
+            startTransaction(async () => {
+                await action()
+            })
+        }}>
+            {children}
+        </button>
+    )
+
+}
+const TabContainer = () => {
+    const [tab, setTab] = useState('about')
+
+    return (
+        <>
+            <TabButton isActive={tab === 'about'} action={() => setTab('about')}>
+                About
+            </TabButton>
+
+            <TabButton isActive={tab === 'posts'} action={() => setTab('posts')}>
+                Posts (slow)
+            </TabButton>
+
+            <TabButton isActive={tab === 'contact'} action={() => setTab('contact')}>
+                Contact
+            </TabButton>
+
+            <hr />
+
+            {tab === 'about' && <AboutTab />}
+            {tab === 'posts' && <PostsTab />}
+            {tab === 'contact' && <ContactTab />}
+        </>
+    )
+}
+const ExposingActionPropFromComponents = () => {
+    return (
+        <>
+            <h3>Exposing Action Prop from Components</h3>
+            <TabContainer />
         </>
     )
 }
